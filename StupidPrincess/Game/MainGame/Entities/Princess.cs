4333f484bce42ca77e2a26b91bb9836bb12fe0ba
Dylan Southern <dyl_654@yahoo.com>
2016-09-06 23:04:11 -0400

    Started Princess movement

--- StupidPrincess/Game/MainGame/Entities/Princess.cs
+++ StupidPrincess/Game/MainGame/Entities/Princess.cs
@@ -32,13 +32,11 @@ namespace StupidPrincess.Game.MainGame.Entities
             DoUpdate();
         }
 
-        private readonly Random _random = new Random();
+        private void DoUpdate()
+        {
+            var pastPosition = _position;
 
-        private void DoUpdate() {
-            var newX = _position.X + _random.Next(-1, 2);
-            var newY = _position.Y + _random.Next(-1, 2);
-
-            var newPosition = new Position(newX, newY);
+            var newPosition = GetNewPosition(pastPosition);
             if (_maze.Bounds.Contains(newPosition) && !PlaceIsOccupied(newPosition)) {
                 _position = newPosition;
             }
@@ -47,5 +45,91 @@ namespace StupidPrincess.Game.MainGame.Entities
         private bool PlaceIsOccupied(Position newPosition) {
             return _maze.Children.Any(c => c.RenderPosition == newPosition);
         }
+
+        private string GetDirection(Position pastPosition)
+        {
+            if (pastPosition == _position) return "start";
+            else if (pastPosition.X < _position.X && pastPosition.Y == _position.Y)
+            {
+                return "right";
+            }
+            else if (pastPosition.X > _position.X && pastPosition.Y == _position.Y)
+            {
+                return "left";
+            }
+            else if (pastPosition.Y < _position.Y && pastPosition.X == _position.X)
+            {
+                return "up";
+            }
+            else
+            {
+                return "down";
+            }
+        }
+
+        private Position MoveDown()
+        {
+            return new Position(_position.X, _position.Y - 1);
+        }
+        private Position MoveUp()
+        {
+            return new Position(_position.X, _position.Y + 1);
+        }
+        private Position MoveLeft()
+        {
+            return new Position(_position.X - 1, _position.Y);
+        }
+        private Position MoveRight()
+        {
+            return new Position(_position.X + 1, _position.Y);
+        }
+        private Position GetNewPosition(Position pastPosition)
+        {
+            if (GetDirection(pastPosition).Equals("start") || GetDirection(pastPosition).Equals("right"))
+            {
+                return MoveRight();
+            }
+            else if (GetDirection(pastPosition).Equals("left"))
+            {
+                return MoveLeft();
+            }
+            else if (GetDirection(pastPosition).Equals("up"))
+            {
+               return MoveUp();
+            }
+            else
+            {
+                return MoveDown();
+            }
+        }
+
+        private int NumberOfDecisions(Position pastPosition)
+        {
+            if (GetDirection(pastPosition).Equals("left") && PlaceIsOccupied(MoveUp()) && PlaceIsOccupied(MoveDown()) && !PlaceIsOccupied(MoveRight()))
+            {
+                return 1;
+            }
+            else if (GetDirection(pastPosition).Equals("right") && PlaceIsOccupied(MoveUp()) && PlaceIsOccupied(MoveDown()) && !PlaceIsOccupied(MoveLeft()))
+            {
+                return 1;
+            }
+            else if (GetDirection(pastPosition).Equals("up") && PlaceIsOccupied(MoveLeft()) && PlaceIsOccupied(MoveRight()) && !PlaceIsOccupied(MoveDown()))
+            {
+                return 1;
+            }
+            else if (GetDirection(pastPosition).Equals("down") && PlaceIsOccupied(MoveLeft()) && PlaceIsOccupied(MoveRight()) && !PlaceIsOccupied(MoveUp()))
+            {
+                return 1;
+            }
+            else
+            {
+                
+            }
+        }
+        private Position MakeDecison(Position pastPosition)
+        {
+            
+        }
+
     }
 }
--- StupidPrincess/Program.cs
+++ StupidPrincess/Program.cs
@@ -8,7 +8,7 @@ namespace StupidPrincess
     {
         public static void Main(string[] args)
         {
-            var renderer = new ConsoleRenderer("Stupid Princess", new Size(128, 52));
+            var renderer = new ConsoleRenderer("Stupid Princess", WindowSizingTool.GetWindowSize());
             var game = new GameState();
             var timeKeeper = new TimeKeeper(10);
             while (!game.ShouldExit()) {
